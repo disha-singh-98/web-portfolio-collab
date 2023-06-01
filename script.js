f38e22586c5da5b7466f176f16d429567a09c8c6
disha-singh-98 <jacksingh80085@gmail.com>
2023-06-01 09:00:00 +0530

    Initial project setup

---
+++ script.js
@@ -0,0 +1,45 @@
+// Modern JavaScript for Web Portfolio
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Web Portfolio loaded successfully!');
+    
+    // Smooth scrolling for navigation links
+    const navLinks = document.querySelectorAll('nav a[href^="#"]');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+    
+    // CTA Button animation
+    const ctaButton = document.querySelector('.cta-button');
+    if (ctaButton) {
+        ctaButton.addEventListener('click', function() {
+            this.style.transform = 'scale(0.95)';
+            setTimeout(() => {
+                this.style.transform = 'scale(1)';
+            }, 150);
+            
+            // Add your action here
+            alert('Welcome to Web Portfolio! 🚀');
+        });
+    }
+    
+    // Add scroll effect to header
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('header');
+        if (window.scrollY > 100) {
+            header.style.background = 'rgba(255, 255, 255, 0.98)';
+        } else {
+            header.style.background = 'rgba(255, 255, 255, 0.95)';
+        }
+    });
+});
